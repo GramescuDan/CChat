a9f73973774b96d96f372ac0243796dfe5a6f637
GramescuDan <gramescudan@yahoo.com>
2022-03-12 18:31:19 +0200

    REALIZATION THAT WE FUCKED UP

--- Client.c
+++ Client.c
@@ -20,72 +20,34 @@ int login(User user) {
     return 0;
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
+
     User *user;
     user = malloc(sizeof(User));
-    if(user==NULL){
+    if (user == NULL) {
         printf("User:malloc()");
         exit(EXIT_FAILURE);
     }
-    user->Username = malloc(50*sizeof(char));
-    if(user->Username==NULL){
+    user->Username = malloc(50 * sizeof(char));
+    if (user->Username == NULL) {
         printf("User->Username:malloc()");
         exit(EXIT_FAILURE);
     }
-    user->Password = malloc(50*sizeof(char));
-    if(user->Password==NULL){
+    user->Password = malloc(50 * sizeof(char));
+    if (user->Password == NULL) {
         printf("User->Password:malloc()");
         exit(EXIT_FAILURE);
     }
 
-    printf("Hello World!\nWelcome to our messaging App!\n");
-    while(1){
-        if(login(*user)==1){
+    printf("Welcome to CChat!\n");
+    while (1) {
+        if (login(*user) == 1) {
             printf("Login Failed!\n Please try again");
-
-        }else{
+            continue;
+        } else {
             user->status = true;
             break;
         }
     }
-    int choice = 0;
-    char yesno[5];
-    char message[256];
-    while(1){
-        printf("1.See Who is Online\n2.Write Message to Chat\n0.LogOut\nYour Choice:");
-        scanf("%d",&choice);
-        switch(choice) {
-            case 0 :
-                printf("\nAre you sure you want to log out?(yes/no)");
-                while(1) {
-                    scanf("%s", &yesno);
-                    if (strcmp(yesno, "yes") == 0) {
-                        user->status = false;
-                        exit(EXIT_SUCCESS);
-                    } else if (strcmp(yesno, "no") == 0) {
-                        break;
-                    } else {
-                        printf("Choice not recognized!\nTry again!\nYour Choice(yes/no):");
-                        continue;
-                    }
-                }
-                break;
-            case 1:
-                //trimitem request catre server ca dorim sa vedem cine e on
-                printf("status:%d",user->status);
-                break;
-            case 2:
-                printf("Your Message:");
-                scanf("%s",&message);
-                //Mesajul scris e trimis catre server.
-                break;
-
-            default :
-                break;
-        }
-        }
-
-    }
-
-
-
+    printf("--CHAT--");
+}
