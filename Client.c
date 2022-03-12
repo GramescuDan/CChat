2d532c14de8bd6726633c45aa407e89d4a54053d
GramescuDan <gramescudan@yahoo.com>
2022-03-12 20:34:40 +0200

    Client setup for server conection

--- Client.c
+++ Client.c
@@ -1,11 +1,13 @@
-
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <intrin.h>
+#include <winsock.h>
 
 typedef struct User{
+    int port;
+    char *ip;
     char *Username;
     char *Password;
     bool status;
@@ -23,11 +25,20 @@ int login(User user) {
 int main(int argc, char* argv[]) {
 
     User *user;
+    struct sockaddr_in server;
+    unsigned int skt=socket(AF_INET,SOCK_STREAM,0);
+
+    if(skt<0){
+        printf("Error:Socket()");
+        exit(EXIT_FAILURE);
+    }
+
     user = malloc(sizeof(User));
     if (user == NULL) {
         printf("User:malloc()");
         exit(EXIT_FAILURE);
     }
+
     user->Username = malloc(50 * sizeof(char));
     if (user->Username == NULL) {
         printf("User->Username:malloc()");
@@ -38,8 +49,20 @@ int main(int argc, char* argv[]) {
         printf("User->Password:malloc()");
         exit(EXIT_FAILURE);
     }
+    user->ip = malloc(9 * sizeof(char));
+    if (user->ip == NULL) {
+        printf("User->Password:malloc()");
+        exit(EXIT_FAILURE);
+    }
+    user->ip = "127.0.0.1";
+    user->port = 8888;
 
-    printf("Welcome to CChat!\n");
+    server.sin_addr.s_addr = inet_addr(user->ip);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(user->port);
+
+
+    printf("Welcome to CChat!\n---Log in---\n");
     while (1) {
         if (login(*user) == 1) {
             printf("Login Failed!\n Please try again");
@@ -49,5 +72,7 @@ int main(int argc, char* argv[]) {
             break;
         }
     }
+
     printf("--CHAT--");
+
 }
