db9b8d37f2c52789c7ae9e5b51abd167a6e13034
dragosremetea <dragos.remetea@gmail.com>
2022-03-14 00:15:09 +0200

    User authorization

--- server.c
+++ server.c
@@ -6,33 +6,86 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <pthread.h>
 #include <sys/types.h>
-#include <signal.h>
 
 #define PORT 4761
-#define MAX_CLIENTS 10
+#define MAX_CLIENTS 5
+#define BUFMAX 4096
+
+typedef enum {True, False} Boolean;
 
 //Client structure
-typedef struct Client {
+typedef struct Connection {
     struct sockaddr_in address;
     int sock_fd;
+} Connection;
+
+typedef struct Client {
     char *username;
     char *password;
+    Connection *connection;
 } Client;
 
+Client clientsDB[MAX_CLIENTS] = {
+    {"admin", "admin"},
+    {"dragos", "dragos"},
+    {"dan", "dan"},
+    {"ioana", "ioana"},
+    {"august", "august"},
+};
 
+void *custom_alloc(int size) {
+    char *mem = (char*)malloc(size);
+    if(mem == NULL) {
+        fprintf(stderr, "Error memory allocation");
+        exit(EXIT_FAILURE);
+    }
+    return (void*)mem;
+}
+
+Connection *connected_clients[MAX_CLIENTS];
+
+Client *verifyCredentials(char *credentials) {
+    char *token = strtok(credentials," ");
+    for(int i =0; i < MAX_CLIENTS; i++) {
+        if (strcmp(clientsDB[i].username, token) == 0) {
+            token = strtok(NULL, " ");
+            if(strcmp(clientsDB[i].password, token) == 0) {
+                Client *client = (Client*)custom_alloc(sizeof(Client));
+                client->username = (char*)custom_alloc(sizeof(strlen(clientsDB[i].username) + 1));
+                client->password = (char*)custom_alloc(sizeof(strlen(clientsDB[i].password) + 1));
+
+                client->connection = (Connection*)custom_alloc(sizeof(Connection));
+
+                strcpy(client->username, clientsDB[i].username);
+                strcpy(client->password, clientsDB[i].password);
+
+                return client;
+            }
+        }
+    }
+    return NULL;
+}
+
+Boolean send_login_confirmation(Client *client) {
+    char *login_message = "1";
+    return send(client->connection->sock_fd, login_message, sizeof(login_message), 0) > 0 ? True : False;
+}
+
+// aloca o zona de memorie cu dimensiune specificata ca parametru
 int main(int argc, char **argv) {
+
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
     unsigned int listen_fd, connection_fd;
+    char buff[BUFMAX];
 
     listen_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (listen_fd < 0) { 
         printf("Error:socket()");
         exit(EXIT_FAILURE);
     }
-    printf("Socket successfully created");
+    printf("Socket successfully created\n");
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -42,33 +95,45 @@ int main(int argc, char **argv) {
         printf("Error:bind()");
         exit(EXIT_FAILURE);
     }
-    printf("Socket successfully bound");
+    printf("Socket successfully bound\n");
 
     if ((listen(listen_fd, 10)) < 0) {
         printf("Error:listen()");
         exit(EXIT_FAILURE);
     }
-    printf("Server listening");
-    printf("   -> CChat <-   ");
+    printf("Server listening\n");
+    printf("   -> CChat <-   \n");
 
     while (1) {
         socklen_t client_length = sizeof(client_addr);
         connection_fd = accept(listen_fd, (struct sockaddr *) &client_addr, &client_length);
 
-        Client *client = (Client *) malloc(sizeof(Client));
-        client->address = client_addr;
-        client->sock_fd = connection_fd;
+        if(connection_fd < 0) {
+            printf("Error:accept");
+            exit(EXIT_FAILURE);
+        }
 
-        client->username = malloc(50 * sizeof(char));
-        if (client->username == NULL) {
-            printf("User->Username:malloc()");
+        int read_message = read(connection_fd, buff, BUFMAX);
+        if(read_message < 0) {
+            printf("Error:read");
             exit(EXIT_FAILURE);
         }
-        client->password = malloc(50 * sizeof(char));
-        if (client->password == NULL) {
-            printf("User->Password:malloc()");
-            exit(EXIT_FAILURE);
+
+        Client *authorized_client = verifyCredentials(buff);
+
+        if(authorized_client == NULL) {
+            // send error message to client
+            // remetea's homework
         }
+
+        Client *client = (Client*)custom_alloc(sizeof(Client));
+        client->connection->address = client_addr;
+        client->connection->sock_fd = connection_fd;
+
+        send_login_confirmation(client);
+
+
+        // NU UITA SA DEALOCI TOATA MEMORIA
     }
 }
 
